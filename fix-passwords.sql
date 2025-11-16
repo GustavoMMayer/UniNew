5b02e80d812e7c1514c1edce8eef2be12a839f72
Felipe Gomes <felipe.gomes@gertec.com.br>
2025-11-16 17:16:02 -0300

    organizalção do projeto

--- fix-passwords.sql
+++
@@ -1,5 +0,0 @@
-UPDATE usuarios 
-SET senha = '$2b$10$.mo3C2sYC5OGvkbZyL.5yeDRe8kqxAwQ5viozr3YnUTHpPhWE79CK' 
-WHERE cpf IN ('11111111111', '22222222222', '33333333333');
-
-SELECT cpf, nome, email, LEFT(senha, 30) as senha_hash FROM usuarios LIMIT 3;
